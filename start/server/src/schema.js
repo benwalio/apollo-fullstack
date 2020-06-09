b4d91e5dd3cbcdc525b435459e38869b207bcf2b
ben <ben.wallace@protonmail.ch>
2020-06-08 21:09:35 -0400

    chugging along with errors

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -33,10 +33,25 @@ const typeDefs = gql`
   }
 
   type Query {
-    launches: [Launch]!
+    launches(
+        """    
+        The number of results to show. Must be >= 1. Default = 20    
+        """    
+        pageSize: Int    
+        """    
+        If you add a cursor here, it will only return results _after_ this cursor    
+        """    
+        after: String  ): 
+    LaunchConnection!
     launch(id: ID!): Launch
     me: User
   }
+
+  type LaunchConnection { # add this below the Query type as an additional type.  
+    cursor: String!  
+    hasMore: Boolean!  
+    launches: [Launch]!
+    }
 
   type Mutation {
     bookTrips(launchIds: [ID]!): TripUpdateResponse!
